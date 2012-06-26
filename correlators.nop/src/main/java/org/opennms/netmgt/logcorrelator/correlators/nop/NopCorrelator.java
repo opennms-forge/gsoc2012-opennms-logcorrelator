c3968d335ab0b53dc797ee38eb52b8bddb8346bb
Dustin Frisch <Dustin Frischfooker@lab.sh>
2012-06-26 17:27:00 +0200

    Cleanup

---
+++ correlators.nop/src/main/java/org/opennms/netmgt/logcorrelator/correlators/nop/NopCorrelator.java
@@ -0,0 +1,13 @@
+package org.opennms.netmgt.logcorrelator.correlators.nop;
+
+import org.opennms.netmgt.logcorrelator.Message;
+import org.opennms.netmgt.logcorrelator.correlators.opennms.AbstractCorrelator;
+
+public class NopCorrelator extends AbstractCorrelator {
+
+	@Override
+	public void process(Message message) {
+		
+	}
+
+}
---
+++ correlators.opennms/src/main/java/org/opennms/netmgt/logcorrelator/correlators/opennms/AbstractCorrelator.java
@@ -0,0 +1,7 @@
+package org.opennms.netmgt.logcorrelator.correlators.opennms;
+
+import org.opennms.netmgt.logcorrelator.Processor;
+
+public abstract class AbstractCorrelator extends Processor {
+
+}
