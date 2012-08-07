89df0520e9f7505565bbd84952a68858b21e8b1d
Dustin Frisch <Dustin Frischfooker@lab.sh>
2012-08-07 16:35:43 +0200

    Empty and useless tests removed

--- correlators.drools/src/test/java/org/opennms/logcorrelator/correlators/drools/DroolsCorrelatorTest.java
+++
@@ -1,9 +0,0 @@
-package org.opennms.logcorrelator.correlators.drools;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
-public class DroolsCorrelatorTest {
-  private final static Logger logger = LoggerFactory.getLogger(DroolsCorrelatorTest.class);
-}
