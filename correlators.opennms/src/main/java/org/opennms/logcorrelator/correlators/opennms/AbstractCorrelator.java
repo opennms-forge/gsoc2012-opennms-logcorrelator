d823e76ea2f96c64967f4509201b8a5ffa65825b
Dustin Frisch <Dustin Frischfooker@lab.sh>
2012-08-07 15:47:45 +0200

    Opennms event bus communication reactivated

--- correlators.opennms/src/main/java/org/opennms/logcorrelator/correlators/opennms/AbstractCorrelator.java
+++ correlators.opennms/src/main/java/org/opennms/logcorrelator/correlators/opennms/AbstractCorrelator.java
@@ -3,11 +3,11 @@ package org.opennms.logcorrelator.correlators.opennms;
 import java.net.UnknownHostException;
 import org.opennms.logcorrelator.api.Correlator;
 import org.opennms.logcorrelator.api.Message;
-//import org.opennms.netmgt.model.events.EventProxy;
-//import org.opennms.netmgt.model.events.EventProxyException;
-//import org.opennms.netmgt.utils.TcpEventProxy;
-//import org.opennms.netmgt.xml.event.Event;
-//import org.opennms.netmgt.xml.event.Logmsg;
+import org.opennms.netmgt.model.events.EventProxy;
+import org.opennms.netmgt.model.events.EventProxyException;
+import org.opennms.netmgt.utils.TcpEventProxy;
+import org.opennms.netmgt.xml.event.Event;
+import org.opennms.netmgt.xml.event.Logmsg;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,35 +15,35 @@ import org.slf4j.LoggerFactory;
 public abstract class AbstractCorrelator implements Correlator {
   private final static Logger logger = LoggerFactory.getLogger(AbstractCorrelator.class);
 
-//  private EventProxy proxy;
+  private EventProxy proxy;
 
   public AbstractCorrelator() {
-//    try {
-//      this.proxy = new TcpEventProxy();
-//
-//    } catch (UnknownHostException e) {
-//      logger.error("Failed to create event proxy", e);
-//    }
+    try {
+      this.proxy = new TcpEventProxy();
+
+    } catch (UnknownHostException e) {
+      logger.error("Failed to create event proxy", e);
+    }
   }
 
   protected void sendEvent(final Message message) {
     logger.debug("Creating OpenNMS event for message: {}", message);
 
-//    Event event = new Event();
-//    event.setUei("uei.opennms.org/logcorrelation/message");
-//
-//    Logmsg logmsg = new Logmsg();
-//    logmsg.setContent(message.toString());
-//    event.setLogmsg(logmsg);
-//
-//    try {
-//      logger.debug("Sending OpenNMS event: {}", event);
-//
-//      this.proxy.send(event);
-//
-//    } catch (EventProxyException e) {
-//      logger.error("Failed to send event", e);
-//    }
+    Event event = new Event();
+    event.setUei("uei.opennms.org/logcorrelation/message");
+
+    Logmsg logmsg = new Logmsg();
+    logmsg.setContent(message.toString());
+    event.setLogmsg(logmsg);
+
+    try {
+      logger.debug("Sending OpenNMS event: {}", event);
+
+      this.proxy.send(event);
+
+    } catch (EventProxyException e) {
+      logger.error("Failed to send event", e);
+    }
   }
 
 }
