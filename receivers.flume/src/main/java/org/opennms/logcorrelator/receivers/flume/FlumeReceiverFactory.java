1743e1cf6dccbd2fd72e1fda934e146be472afaa
Dustin Frisch <Dustin Frischfooker@lab.sh>
2012-08-13 17:53:49 +0200

    Factory for flume receiver added

---
+++ receivers.flume/src/main/java/org/opennms/logcorrelator/receivers/flume/FlumeReceiverFactory.java
@@ -0,0 +1,26 @@
+package org.opennms.logcorrelator.receivers.flume;
+
+import org.opennms.logcorrelator.api.MessageFactory;
+import org.opennms.logcorrelator.api.Pipeline;
+import org.opennms.logcorrelator.api.Receiver;
+import org.opennms.logcorrelator.api.ReceiverFactory;
+import org.opennms.logcorrelator.config.xml.ReceiverConfiguration;
+
+
+public class FlumeReceiverFactory implements ReceiverFactory {
+  @Override
+  public Receiver create(final String id,
+                         final MessageFactory messageFactory,
+                         final Pipeline pipeline,
+                         final ReceiverConfiguration configuration) {
+    final String host = configuration.getProperty("host");
+    final int port = Integer.parseInt(configuration.getProperty("port"));
+
+    return new FlumeReceiver(id,
+                             messageFactory,
+                             pipeline,
+                             host,
+                             port);
+  }
+
+}
